a3819dcd347291fbca55844d0aeb892ab2c71036
ian5757 <103472858+ian5757@users.noreply.github.com>
2023-03-17 17:06:28 +0900

    Add files via upload

---
+++ baby_bot/mini_bot/src/pinklab_minibot_robot/minibot_indoor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'minibot_indoor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='in',
+    maintainer_email='in@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'obstacle_detection = minibot_indoor.obstacle_detection:main'
+        ],
+    },
+)
