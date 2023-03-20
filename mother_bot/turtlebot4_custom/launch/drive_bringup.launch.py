7562ab404e4002ee2c6a145141bd9e217c475f24
정재욱 <80450771+JAEWOOK6488@users.noreply.github.com>
2023-03-21 06:40:43 +0900

    changed executable file names

--- mother_bot/turtlebot4_custom/launch/drive_bringup.launch.py
+++ mother_bot/turtlebot4_custom/launch/drive_bringup.launch.py
@@ -9,11 +9,15 @@ def generate_launch_description():
                  output = 'screen',
             ),
             Node(package = 'turtlebot4_custom',
-                 executable = 'object_detector',
+                 executable = 'obs_detector',
                  output = 'screen'
             ),
             Node(package = 'turtlebot4_custom',
                  executable= 'lane_detector',
+                 output = 'screen',
+            ),
+            Node(package = 'turtlebot4_custom',
+                 executable= 'aruco_marker',
                  output = 'screen',
             ),
         ]
